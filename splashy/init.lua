25ab2f16e017c8dc2ad8346301020edfa850eaae
Ruairidh Carmichael <ruairidhcarmichael@live.co.uk>
2015-01-19 15:03:40 +0000

    Basic functionality now.

--- splashy/init.lua
+++ splashy/init.lua
@@ -3,8 +3,11 @@ local tween = require(path .. '/tween')
 
 local splashy = {}
 splashy.list = {}
-splashy.alpha = 255
+splashy.inalpha = 0
+splashy.outalpha = 255
 splashy.count = 1
+splashy.tweenlist = {}
+splashy.fadestate = "in"
 
 function splashy.addSplash(image, index)
 
@@ -15,13 +18,23 @@ function splashy.addSplash(image, index)
 
 	print(splashy.list[index])
 
+	splashy.tweenlist[index] = tween.new(2, splashy, {inalpha = 255, outalpha = 0})
+
 end
 
 function splashy.draw()
 
 	for i=1, #splashy.list do
 
-		love.graphics.setColor(255, 255, 255, splashy.alpha)
+		if splashy.fadestate == "in" then
+
+			love.graphics.setColor(255, 255, 255, splashy.inalpha)
+
+		elseif splashy.fadestate == "out" then
+
+			love.graphics.setColor(255, 255, 255, splashy.outalpha)
+
+		end
 
 		-- If the current splash is the one in the list.
 
@@ -36,13 +49,47 @@ function splashy.draw()
 			local centerimagewidth = splash:getWidth() / 2
 			local centerimageheight = splash:getHeight() / 2
 
-			love.graphics.draw(splashy.list[i], centerwidth - centerimagewidth, centerheight - centerimageheight)
+			love.graphics.draw(splash, centerwidth - centerimagewidth, centerheight - centerimageheight)
 
 		end
 
 	end
 
 	love.graphics.setColor(255, 255, 255, 255)
+
+end
+
+function splashy.update(dt)
+
+	for i=1, #splashy.tweenlist do
+
+		if splashy.count == i then
+
+			local tweenComplete = splashy.tweenlist[i]:update(dt)
+
+			if tweenComplete then
+
+				if splashy.fadestate == "in" then
+
+					splashy.tweenlist[i]:reset()
+
+					splashy.fadestate = "out"
+
+				elseif splashy.fadestate == "out" then
+
+					splashy.tweenlist[i]:reset()
+
+					splashy.count = splashy.count + 1
+
+					splashy.fadestate = "in"
+
+				end
+
+			end
+
+		end
+
+	end
 
 end
 
